83826f44c49a1fa0db9c9549b215a2701280e83c
p k <kbprabu6@gmail.com>
2022-10-04 15:26:55 +0530

    second commit

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -2,6 +2,18 @@ import React from "react";
 import "../Styles/Sidebar.css";
 import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
 import CreateIcon from '@mui/icons-material/Create';
+import SidebarOption from "./SidebarOption";
+import InsertCommentIcon from '@mui/icons-material/InsertComment';
+import InboxIcon from '@mui/icons-material/Inbox';
+import DraftsIcon from '@mui/icons-material/Drafts';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import AppsIcon from '@mui/icons-material/Apps';
+import FileCopyIcon from '@mui/icons-material/FileCopy';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import AddIcon from '@mui/icons-material/Add';
+
 
 function sidebar() {
   return (
@@ -15,8 +27,21 @@ function sidebar() {
           </h3>
         </div>
         <CreateIcon/>
-        <SidebarOption />
       </div>
+        <SidebarOption Icon={InsertCommentIcon} title="Threads" />
+        <SidebarOption Icon={InboxIcon} title="Mention & reactions" />
+        <SidebarOption Icon={DraftsIcon} title="Saved items" />
+        <SidebarOption Icon={BookmarkIcon} title="Channel browser" />
+        <SidebarOption Icon={PeopleAltIcon} title="People & user groups" />
+        <SidebarOption Icon={AppsIcon} title="Apps" />
+        <SidebarOption Icon={FileCopyIcon} title="File browser" />
+        <SidebarOption Icon={ExpandLessIcon} title="Show less" />
+        <hr/>
+        <SidebarOption Icon={ExpandMoreIcon} title="Channel" />
+        <hr/>
+        <SidebarOption Icon={AddIcon} title="Add" />
+
+        {/* after connecting to dB it will list all the channels <SidebarOption.../>........*/}
     </div>
   );
 }
